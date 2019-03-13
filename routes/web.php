d2e4fd874dc4a60ed8e77523a95ac1c5a6cbe41c
Narmeen-Adel <memaadel24@gmail.com>
2019-03-14 00:00:55 +0200

    update==>route

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@
 Route::get('/posts','PostController@index')->name('posts.index');
 Route::get('/posts/create','PostController@create')->name('posts.create');
 Route::post('/posts','PostController@store')->name('posts.store');
-Route::post('/posts/{post}/edit','PostController@edit')->name('posts.edit');
-Route::post('/posts/post','PostController@update')->name('posts.update');
+Route::get('/posts/{post}/edit','PostController@edit')->name('posts.edit');
+Route::put('/posts/post','PostController@update')->name('posts.update');
+Route::get('/posts/post','PostController@show')->name('posts.show');
